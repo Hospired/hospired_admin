6d80e0d476162a9d8104c27471057d54c506acfd
Jonathan <26martinez.508@gmail.com>
2025-09-27 14:19:28 -0600

    Last theme provider configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
