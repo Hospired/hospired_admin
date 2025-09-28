d29197ca5adfad8ddab862eceed9d271c1e5487b
Ernesto <ernesto.vivas@outlook.com>
2025-09-28 15:37:36 -0600

    Define and call signIn and signOut in API service

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,53 +1,36 @@
-/*import { AuthGuard } from "@/components/auth/AuthGuard"
-import { AppSidebar } from "@/components/app-sidebar"
-    import {
-    SidebarInset,
-    SidebarProvider
-} from "@/components/ui/sidebar"
-
-    export default function Page({children}:{ children: React.ReactNode}) {
-    return (
-        <AuthGuard>
-            <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                {children}
-            </SidebarInset>
-        </SidebarProvider>
-        </AuthGuard>
-    )
-}*/
-
-import { Navbar14 } from "@/components/Navbar/Navbar"
-import { AuthGuard } from "@/components/auth/AuthGuard"
-import type React from "react"
-import { AppSidebar } from "@/components/app-sidebar"
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { Separator } from "@/components/ui/separator"
+import { Navbar14 } from "@/components/Navbar/Navbar";
+import { AuthGuard } from "@/components/auth/AuthGuard";
+import type React from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 
 export default function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <AuthGuard>
-            <SidebarProvider>
+  return (
+    <AuthGuard>
+      <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-            <header className="flex h-16 min-h-[64px] z-10 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <header className="flex h-16 min-h-[64px] z-10 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4 w-full">
-                <SidebarTrigger className="-ml-1" />
-                <div className="relative flex-1">
+              <SidebarTrigger className="-ml-1" />
+              <div className="relative flex-1">
                 <Separator orientation="vertical" className="mr-2 h-4" />
                 <Navbar14 />
-                </div>
+              </div>
             </div>
-            </header>
-            <div className="flex flex-1 flex-col gap-4 p-4 pt-5">{children}</div>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-5">{children}</div>
         </SidebarInset>
-        </SidebarProvider>
-        </AuthGuard>
-        
-    )
+      </SidebarProvider>
+    </AuthGuard>
+  );
 }
--- src/backend-api/apiService.ts
+++ src/backend-api/apiService.ts
@@ -16,3 +16,26 @@ export async function signUpUser(email: string, password: string) {
 
   return data;
 }
+
+export async function signInUser(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+  });
+
+  if (error) {
+    console.error("Signin error:", error.message);
+    throw error;
+  }
+
+  return data;
+}
+
+export async function signOutUser() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error("Signout error:", error.message);
+    throw error;
+  }
+}
